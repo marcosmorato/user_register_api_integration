ba8695aa60472ed5175b2d26e65f25aeead25c76
Marcos R. Morato <marcos.nyrou@hotmail.com>
2020-11-20 15:45:38 -0300

    authentication is complete

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import "./App.css";
-import { Switch, Route, Link } from "react-router-dom";
-import { useState } from "react";
-import Login from "./pages/login";
-import Register from "./pages/register";
+import Authenticator from "./components/authenticator";
 
 const App = () => {
-  const [localStorage, setLocalStorage] = useState(false);
-  const [registerStorage, setRegisterStorage] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <ul>
-          <li>
-            <Link to="/register">Cadastro</Link>
-          </li>
-          <li>
-            <Link to="/">Login</Link>
-          </li>
-        </ul>
-
-        <Switch>
-          <Route path="/register">
-            <Register
-              registerStorage={registerStorage}
-              setRegister={setRegisterStorage}
-            />
-          </Route>
-
-          <Route path="/">
-            <Login login={localStorage} setLocalStorage={setLocalStorage} />
-          </Route>
-        </Switch>
+        <Authenticator />
       </header>
     </div>
   );
