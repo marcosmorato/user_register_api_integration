aabe892ed042996748fb8b7bbb83303e0792d69f
Gabriel Fabiano <gabrielfabianoo91@gmail.com>
2020-11-21 18:51:03 -0300

    fix error when creator is null

--- src/components/feedback/index.jsx
+++ src/components/feedback/index.jsx
@@ -1,11 +1,21 @@
 const Feedback = (props) => {
+  console.log(props.creator);
+
   return (
     <div>
       <span>{props.id} </span>
       <span> {props.name} </span>
       <span> {props.comment} </span>
-      <span> {props.creator.name} </span>
-      <span> {props.creator.user} </span>
+      {props.creator ? (
+        <span> {props.creator.name} </span>
+      ) : (
+        <div>my creator don't have name</div>
+      )}
+      {props.creator ? (
+        <span> {props.creator.user} </span>
+      ) : (
+        <div>my creator don't have name</div>
+      )}
       <span> {props.grade} </span>
     </div>
   );
